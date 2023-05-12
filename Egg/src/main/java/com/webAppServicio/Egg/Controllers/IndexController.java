4740f099b63e82a7edf82c74044dcdca0a2d8d16
EliasGiova <eliasgiooovanella@gmail.com>
2023-05-11 21:20:05 -0300

    probando

--- Egg/src/main/java/com/webAppServicio/Egg/Controllers/IndexController.java
+++ Egg/src/main/java/com/webAppServicio/Egg/Controllers/IndexController.java
@@ -1,18 +1,16 @@
 package com.webAppServicio.Egg.Controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-/**
- *
- * @author elias
- */
 @Controller
-public class MyController {
+@RequestMapping("/")
+public class IndexController {
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(ModelMap modelo) {
         return "index.html";
     }
 }
