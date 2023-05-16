4c973719075ec8252ee808624c9176a096d9cebe
EliasGiova <eliasgiooovanella@gmail.com>
2023-05-16 13:07:28 -0300

    mejoras de vistas, y controladores user y supplier

--- Egg/src/main/java/com/webAppServicio/Egg/Repositories/UserRepository.java
+++ Egg/src/main/java/com/webAppServicio/Egg/Repositories/UserRepository.java
@@ -1,9 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package com.webAppServicio.Egg.repositories;
+package com.webAppServicio.Egg.Repositories;
 
 import com.webAppServicio.Egg.Entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,13 +6,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-/**
- *
- * @author Juan Cruz
- */
-
 @Repository
-
 public interface UserRepository extends JpaRepository<User, String>{
     
     @Query("SELECT u FROM User u WHERE u.nombre = :nombre")
