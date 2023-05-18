5828b9def5c8f4d78db654794fab0252033b9367
EliasGiova <eliasgiooovanella@gmail.com>
2023-05-18 20:49:39 -0300

    problema resuelto

--- Egg/src/main/java/com/webAppServicio/Egg/Repositories/ImageRepository.java
+++ Egg/src/main/java/com/webAppServicio/Egg/Repositories/ImageRepository.java
@@ -1,17 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.webAppServicio.Egg.Repositories;
 
 import com.webAppServicio.Egg.Entities.Image;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-/**
- *
- * @author elias
- */
 @Repository
 public interface ImageRepository extends JpaRepository<Image, String>{
     
